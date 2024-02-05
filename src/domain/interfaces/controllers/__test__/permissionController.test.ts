f73c1022a4e79ae184728182e5f9f29159be9acf
BrunoCiprianoDev <cipriano990@gmail.com>
2024-02-05 09:03:28 -0300

    feat: build all controllers

--- src/domain/interfaces/controllers/__test__/permissionController.test.ts
+++ src/domain/interfaces/controllers/__test__/permissionController.test.ts
@@ -3,216 +3,211 @@ import { IHttpContext } from '../../adapters/httpContext';
 import { IPermissionController, PermissionController } from '../auth/permissionController';
 
 describe('PermissionController tests', () => {
-    let mockedPermissionUseCase: jest.Mocked<IPermissionUseCases>;
-    let mockedHttpContext: jest.Mocked<IHttpContext>;
-    let permissionController: IPermissionController;
+  let mockedPermissionUseCase: jest.Mocked<IPermissionUseCases>;
+  let mockedHttpContext: jest.Mocked<IHttpContext>;
+  let permissionController: IPermissionController;
 
-    beforeAll(() => {
-        mockedPermissionUseCase = {
-            createPermissions: jest.fn(),
-            existsPermissions: jest.fn(),
-            findPermissionsByUser: jest.fn(),
-            deletePermission: jest.fn(),
-        };
+  beforeAll(() => {
+    mockedPermissionUseCase = {
+      createPermissions: jest.fn(),
+      existsPermissions: jest.fn(),
+      findPermissionsByUser: jest.fn(),
+      deletePermission: jest.fn(),
+    };
 
-        mockedHttpContext = {
-            getRequest: jest.fn(),
-            send: jest.fn(),
-        };
+    mockedHttpContext = {
+      getRequest: jest.fn(),
+      send: jest.fn(),
+    };
 
-        permissionController = new PermissionController(mockedPermissionUseCase);
+    permissionController = new PermissionController(mockedPermissionUseCase);
+  });
+
+  describe('CreatePermission tests', () => {
+    test('Should return a permission created successfully', async () => {
+      const permissionExpected = {
+        id: 'uuid',
+        userId: 'userId',
+        role: 'ADMIN',
+      };
+
+      jest.spyOn(mockedPermissionUseCase, 'createPermissions').mockResolvedValue([permissionExpected]);
+
+      (mockedHttpContext.getRequest as jest.Mock).mockReturnValue({
+        headers: { any: '' },
+        body: {
+          userId: 'userId',
+          role: 'ADMIN',
+        },
+      });
+
+      await permissionController.createPermission(mockedHttpContext);
+
+      expect(mockedHttpContext.send).toHaveBeenCalledWith({
+        statusCode: 201,
+        body: [
+          {
+            id: 'uuid',
+            userId: 'userId',
+            role: 'ADMIN',
+          },
+        ],
+      });
+      expect(mockedPermissionUseCase.createPermissions).toHaveBeenCalledWith([
+        {
+          userId: 'userId',
+          role: 'ADMIN',
+        },
+      ]);
     });
 
-    describe('CreatePermission tests', () => {
-        test('Should return a permission created successfully', async () => {
-            const permissionExpected = {
-                id: 'uuid',
-                userId: 'userId',
-                role: 'ADMIN',
-            };
+    test('Must handle errors occurring in the request', async () => {
+      jest.spyOn(mockedPermissionUseCase, 'createPermissions').mockRejectedValue(new Error('Any string'));
 
-            jest.spyOn(mockedPermissionUseCase, 'createPermissions').mockResolvedValue([permissionExpected]);
+      (mockedHttpContext.getRequest as jest.Mock).mockReturnValue({
+        headers: { any: '' },
+        body: {
+          userId: 'userId',
+          role: 'ADMIN',
+        },
+      });
 
-            (mockedHttpContext.getRequest as jest.Mock).mockReturnValue({
-                headers: { any: '' },
-                body: {
-                    userId: 'userId',
-                    role: 'ADMIN',
-                },
-            });
+      await permissionController.createPermission(mockedHttpContext);
 
-            await permissionController.createPermission(mockedHttpContext);
-
-            expect(mockedHttpContext.send).toHaveBeenCalledWith({
-                statusCode: 201,
-                body: [
-                    {
-                        id: 'uuid',
-                        userId: 'userId',
-                        role: 'ADMIN',
-                    },
-                ],
-            });
-            expect(mockedPermissionUseCase.createPermissions).toHaveBeenCalledWith([
-                {
-                    userId: 'userId',
-                    role: 'ADMIN',
-                },
-            ]);
-        });
-
-        test('Must handle errors occurring in the request', async () => {
-            jest.spyOn(mockedPermissionUseCase, 'createPermissions').mockRejectedValue(new Error('Any string'));
-
-            (mockedHttpContext.getRequest as jest.Mock).mockReturnValue({
-                headers: { any: '' },
-                body: {
-                    userId: 'userId',
-                    role: 'ADMIN',
-                },
-            });
-
-            await permissionController.createPermission(mockedHttpContext);
-
-            expect(mockedHttpContext.send).toHaveBeenCalledWith({
-                statusCode: 500,
-                body: { message: 'Unexpected error occurred' },
-            });
-            expect(mockedPermissionUseCase.createPermissions).toHaveBeenCalledWith([
-                {
-                    userId: 'userId',
-                    role: 'ADMIN',
-                },
-            ]);
-        });
-
-        test('Should handle uniformed attributes', async () => {
-            (mockedHttpContext.getRequest as jest.Mock).mockReturnValue({
-                headers: { any: '' },
-                body: {},
-            });
-
-            await permissionController.createPermission(mockedHttpContext);
-
-            expect(mockedPermissionUseCase.createPermissions).toHaveBeenCalledWith([
-                {
-                    userId: '',
-                    role: '',
-                },
-            ]);
-        });
+      expect(mockedHttpContext.send).toHaveBeenCalledWith({
+        statusCode: 500,
+        body: { message: 'Unexpected error occurred' },
+      });
+      expect(mockedPermissionUseCase.createPermissions).toHaveBeenCalledWith([
+        {
+          userId: 'userId',
+          role: 'ADMIN',
+        },
+      ]);
     });
 
-    describe('FindPermissionsByUser tests', () => {
-        test('Should return list Permissions by user Id', async () => {
-            const permissionExpected = [
-                {
-                    id: 'uuid',
-                    userId: 'userId',
-                    role: 'ADMIN',
-                },
-            ];
+    test('Should handle uniformed attributes', async () => {
+      (mockedHttpContext.getRequest as jest.Mock).mockReturnValue({
+        headers: { any: '' },
+        body: {},
+      });
 
-            jest.spyOn(mockedPermissionUseCase, 'findPermissionsByUser').mockResolvedValue(permissionExpected);
+      await permissionController.createPermission(mockedHttpContext);
 
-            (mockedHttpContext.getRequest as jest.Mock).mockReturnValue({
-                headers: { any: '' },
-                body: {},
-                params: { userId: 'userId' },
-            });
+      expect(mockedPermissionUseCase.createPermissions).toHaveBeenCalledWith([
+        {
+          userId: '',
+          role: '',
+        },
+      ]);
+    });
+  });
 
-            await permissionController.findPermissionsByUser(mockedHttpContext);
+  describe('FindPermissionsByUser tests', () => {
+    test('Should return list Permissions by user Id', async () => {
+      const permissionExpected = [
+        {
+          id: 'uuid',
+          userId: 'userId',
+          role: 'ADMIN',
+        },
+      ];
 
-            expect(mockedHttpContext.send).toHaveBeenCalledWith({
-                statusCode: 200,
-                body: [
-                    {
-                        id: 'uuid',
-                        userId: 'userId',
-                        role: 'ADMIN',
-                    },
-                ],
-            });
-            expect(mockedPermissionUseCase.findPermissionsByUser).toHaveBeenCalledWith('userId');
-        });
+      jest.spyOn(mockedPermissionUseCase, 'findPermissionsByUser').mockResolvedValue(permissionExpected);
 
-        test('Should handle uniformed attributes', async () => {
-            (mockedHttpContext.getRequest as jest.Mock).mockReturnValue({
-                headers: { any: '' },
-                body: {},
-            });
+      (mockedHttpContext.getRequest as jest.Mock).mockReturnValue({
+        headers: { any: '' },
+        body: {},
+        params: { userId: 'userId' },
+      });
 
-            await permissionController.findPermissionsByUser(mockedHttpContext);
+      await permissionController.findPermissionsByUser(mockedHttpContext);
 
-            expect(mockedPermissionUseCase.findPermissionsByUser).toHaveBeenCalledWith('');
-        });
-
-        test('Must handle errors occurring in the request', async () => {
-            jest.spyOn(mockedPermissionUseCase, 'findPermissionsByUser').mockRejectedValue(new Error('Any string'));
-
-            (mockedHttpContext.getRequest as jest.Mock).mockReturnValue({
-                headers: { any: '' },
-                body: {},
-                params: { userId: 'userId' },
-            });
-
-            await permissionController.findPermissionsByUser(mockedHttpContext);
-
-            expect(mockedHttpContext.send).toHaveBeenCalledWith({
-                statusCode: 500,
-                body: { message: 'Unexpected error occurred' },
-            });
-        });
+      expect(mockedHttpContext.send).toHaveBeenCalledWith({
+        statusCode: 200,
+        body: [
+          {
+            id: 'uuid',
+            userId: 'userId',
+            role: 'ADMIN',
+          },
+        ],
+      });
+      expect(mockedPermissionUseCase.findPermissionsByUser).toHaveBeenCalledWith('userId');
     });
 
-    describe('DeletePermission tests', () => {
+    test('Should handle uniformed attributes', async () => {
+      (mockedHttpContext.getRequest as jest.Mock).mockReturnValue({
+        headers: { any: '' },
+        body: {},
+      });
 
-        test('Should delete a permission by userId and role', async () => {
+      await permissionController.findPermissionsByUser(mockedHttpContext);
 
-            jest.spyOn(mockedPermissionUseCase, 'deletePermission').mockResolvedValue();
+      expect(mockedPermissionUseCase.findPermissionsByUser).toHaveBeenCalledWith('');
+    });
 
-            (mockedHttpContext.getRequest as jest.Mock).mockReturnValue({
-                headers: { any: '' },
-                body: { userId: 'userId', role: 'STANDARD' },
-            });
+    test('Must handle errors occurring in the request', async () => {
+      jest.spyOn(mockedPermissionUseCase, 'findPermissionsByUser').mockRejectedValue(new Error('Any string'));
 
-            await permissionController.deletePermission(mockedHttpContext);
+      (mockedHttpContext.getRequest as jest.Mock).mockReturnValue({
+        headers: { any: '' },
+        body: {},
+        params: { userId: 'userId' },
+      });
 
-            expect(mockedHttpContext.send).toHaveBeenCalledWith({
-                statusCode: 204,
-                body: {},
-            });
-            expect(mockedPermissionUseCase.deletePermission).toHaveBeenCalledWith('userId', 'STANDARD');
+      await permissionController.findPermissionsByUser(mockedHttpContext);
 
-        })
+      expect(mockedHttpContext.send).toHaveBeenCalledWith({
+        statusCode: 500,
+        body: { message: 'Unexpected error occurred' },
+      });
+    });
+  });
 
-        test('Should handle uniformed attributes', async () => {
-            (mockedHttpContext.getRequest as jest.Mock).mockReturnValue({
-                headers: { any: '' },
-                body: {},
-            });
+  describe('DeletePermission tests', () => {
+    test('Should delete a permission by userId and role', async () => {
+      jest.spyOn(mockedPermissionUseCase, 'deletePermission').mockResolvedValue();
 
-            await permissionController.deletePermission(mockedHttpContext);
+      (mockedHttpContext.getRequest as jest.Mock).mockReturnValue({
+        headers: { any: '' },
+        body: { userId: 'userId', role: 'STANDARD' },
+      });
 
-            expect(mockedPermissionUseCase.deletePermission).toHaveBeenCalledWith('', '');
-        });
+      await permissionController.deletePermission(mockedHttpContext);
 
-        test('Must handle errors occurring in the request', async () => {
-            jest.spyOn(mockedPermissionUseCase, 'deletePermission').mockRejectedValue(new Error('Any string'));
+      expect(mockedHttpContext.send).toHaveBeenCalledWith({
+        statusCode: 204,
+        body: {},
+      });
+      expect(mockedPermissionUseCase.deletePermission).toHaveBeenCalledWith('userId', 'STANDARD');
+    });
 
-            (mockedHttpContext.getRequest as jest.Mock).mockReturnValue({
-                headers: { any: '' },
-                body: { userId: 'userId', role: 'STANDARD' },
-            });
+    test('Should handle uniformed attributes', async () => {
+      (mockedHttpContext.getRequest as jest.Mock).mockReturnValue({
+        headers: { any: '' },
+        body: {},
+      });
 
-            await permissionController.deletePermission(mockedHttpContext);
+      await permissionController.deletePermission(mockedHttpContext);
 
-            expect(mockedHttpContext.send).toHaveBeenCalledWith({
-                statusCode: 500,
-                body: { message: 'Unexpected error occurred' },
-            });
-        });
+      expect(mockedPermissionUseCase.deletePermission).toHaveBeenCalledWith('', '');
+    });
 
+    test('Must handle errors occurring in the request', async () => {
+      jest.spyOn(mockedPermissionUseCase, 'deletePermission').mockRejectedValue(new Error('Any string'));
 
-    })
+      (mockedHttpContext.getRequest as jest.Mock).mockReturnValue({
+        headers: { any: '' },
+        body: { userId: 'userId', role: 'STANDARD' },
+      });
+
+      await permissionController.deletePermission(mockedHttpContext);
+
+      expect(mockedHttpContext.send).toHaveBeenCalledWith({
+        statusCode: 500,
+        body: { message: 'Unexpected error occurred' },
+      });
+    });
+  });
 });
